8783b451f154f22e2c2b39e35b4fe061baf25456
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-10-25 16:46:10 -0300

    chore: alters the route and its return

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/teste3', (req, res) => {
-    return res.json({ hello: 'world 3' })
+app.get('/teste', (req, res) => {
+    return res.json({ hello: 'world' })
 })
 
 app.listen(3333)
